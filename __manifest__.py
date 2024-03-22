96f31e27ef62d45b6131613ebaf1370b9e67f6c1
Abdul karim Osman <a.osman@bab.com.sa>
2024-03-22 22:46:34 +0300

    first localhost

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2016-2017 LasLabs Inc.
+# Copyright 2017-2018 Tecnativa - Jairo Llopis
+# Copyright 2018-2019 Tecnativa - Alexandre Díaz
+# Copyright 2021 ITerra - Sergey Shebanin
+# Copyright 2023 Onestein - Anjeel Haria
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "gym system ",
+    
+}
